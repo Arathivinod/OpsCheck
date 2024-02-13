45501cd84ec67c9161830886aa64c56c086afa91
Arathivinod <arathivinodwrk@gmail.com>
2024-02-13 12:12:26 +0530

    modals for events added

--- lib/modal/modal_events.dart
+++ lib/modal/modal_events.dart
@@ -1,8 +1,9 @@
+// Model class representing an Event
 class Event {
-  final String eventName;
-  final String category;
-  final String date;
-  final String time;
+  final String eventName; // Name of the event
+  final String category; // Category of the event
+  final String date; // Date of the event
+  final String time; // Time of the event
 
   Event({
     required this.eventName,
@@ -11,30 +12,35 @@ class Event {
     required this.time,
   });
 
+  // Factory method to create an Event object from JSON data
   factory Event.fromJson(Map<String, dynamic> json) {
     return Event(
-      eventName: json['eventName'],
-      category: json['category'],
-      date: json['date'],
-      time: json['time'],
+      eventName: json['eventName'], // Extract eventName from JSON
+      category: json['category'], // Extract category from JSON
+      date: json['date'], // Extract date from JSON
+      time: json['time'], // Extract time from JSON
     );
   }
 }
+
+// Model class representing a response containing a list of events
 class EventResponse {
-  final String status;
-  final List<Event> events;
+  final String status; // Status of the response
+  final List<Event> events; // List of events in the response
 
   EventResponse({
     required this.status,
     required this.events,
   });
 
+  // Factory method to create an EventResponse object from JSON data
   factory EventResponse.fromJson(Map<String, dynamic> json) {
     return EventResponse(
-      status: json['status'],
-      events: (json['data']['events'] as List)
-          .map((eventJson) => Event.fromJson(eventJson))
-          .toList(),
+      status: json['status'], // Extract status from JSON
+      events: (json['data']['events'] as List) // Extract events list from JSON
+          .map((eventJson) =>
+              Event.fromJson(eventJson)) // Map JSON to Event objects
+          .toList(), // Convert to a list of Event objects
     );
   }
 }
