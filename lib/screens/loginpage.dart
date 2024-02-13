caf931cdabdc639158c57b1ec2bd8abaaa69ba77
Arathivinod <arathivinodwrk@gmail.com>
2024-02-13 10:55:46 +0530

    added basic validations

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,59 +1,103 @@
 import 'package:flutter/material.dart';
+import 'event_list.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? _employeeId;
+  String? _password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Center(
-              child: Text(
-                'OpsCheck',
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 80),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Employee ID',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: () {
-                // Login button pressed
-              },
-              icon: Icon(Icons.login),
-              label: Text('Login'),
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Center(
+                child: Text(
+                  'OpsCheck',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                backgroundColor: MaterialStateProperty.all(Colors.blue),
               ),
-            ),
-          ],
+              const SizedBox(height: 80),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Employee ID',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter Employee ID';
+                  }
+                  if (value.length > 4) {
+                    return 'Employee ID must not exceed 4 digits';
+                  }
+                  if (int.tryParse(value) == null) {
+                    return 'Employee ID must be a number';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _employeeId = value,
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  if (value.length < 4) {
+                    return 'Password must be at least 4 characters long';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => EventListScreen()),
+                    );
+                  }
+                },
+                icon: const Icon(Icons.login),
+                label: const Text('Login'),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
