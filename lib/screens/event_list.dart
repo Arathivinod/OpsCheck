ed145c324a0de35c28ccafc44f04a166c67541e9
Arathivinod <arathivinodwrk@gmail.com>
2024-02-19 17:59:02 +0530

    UI changes made like reduced no of clicks

--- lib/screens/event_list.dart
+++ lib/screens/event_list.dart
@@ -1,33 +1,224 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:opscheck/modal/modal_events.dart'; // Importing Event model
-import 'package:intl/intl.dart'; // Importing date formatting utilities
-import 'participant_list.dart'; // Importing ParticipantListScreen
+import 'package:intl/intl.dart';
+import 'package:opscheck/modal/modal.dart'; // Importing Event model
+import 'participant_list.dart';
 
-//Listing events with respect to current date
 class EventListScreen extends StatefulWidget {
-  const EventListScreen({super.key});
+  const EventListScreen({Key? key}) : super(key: key);
 
   @override
   _EventListScreenState createState() => _EventListScreenState();
 }
 
 class _EventListScreenState extends State<EventListScreen> {
-  List<Event> _events = [];
-  List<Event> _newEvents = [];
-  DateTime _selectedDate = DateTime.now(); // Initialize with current date
-  ValueNotifier<bool> _isLoading =
-      ValueNotifier(false); // Flag to track whether events are being loaded
+  late List<EventByDate> _eventsByDate = [];
+  late DateTime _selectedDate = DateTime.now();
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _fetchEventsForSelectedDate(
-        _selectedDate); // Fetch events for selected date when screen initializes
+    _fetchEventsByDate(_selectedDate);
+    print("Current Date: ${DateFormat('yyyy-MM-dd').format(_selectedDate)}");
   }
 
-  // Function to format the time
+  DateTime dateOnly(DateTime dateTime) {
+    return DateTime(dateTime.year, dateTime.month, dateTime.day);
+  }
+
+  Future<void> _fetchEventsByDate(DateTime date) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final response = await http
+          .get(Uri.parse('http://10.0.2.2:3000/api/v1/analytics?limit=5000'));
+
+      print('API Response: ${response.statusCode} ${response.body}');
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final eventResponse = EventResponse.fromJson(responseData);
+
+        setState(() {
+          _eventsByDate = eventResponse.eventsWithParticipantsByDate
+              .where((eventByDate) =>
+                  _isSameDay(dateOnly(eventByDate.date), dateOnly(date)))
+              .toList();
+          _isLoading = false;
+        });
+
+        print("Filtered events: $_eventsByDate");
+        print("Selected Date: $date");
+      } else {
+        throw Exception(
+            'Failed to fetch events. Status Code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching events: $error');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  bool _isSameDay(DateTime date1, DateTime date2) {
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null && pickedDate != _selectedDate) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+      await _fetchEventsByDate(_selectedDate);
+      print("Selected Date: ${DateFormat('yyyy-MM-dd').format(_selectedDate)}");
+    }
+  }
+
+  void _nextDate() async {
+    setState(() {
+      _selectedDate = _selectedDate.add(Duration(days: 1));
+    });
+    await _fetchEventsByDate(_selectedDate);
+  }
+
+  void _previousDate() async {
+    setState(() {
+      _selectedDate = _selectedDate.subtract(Duration(days: 1));
+    });
+    await _fetchEventsByDate(_selectedDate);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Event List',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue, // App bar color
+      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: _previousDate,
+                        icon: Icon(Icons.arrow_back),
+                        color: Colors.blue,
+                      ),
+                      ElevatedButton(
+                        onPressed: () => _selectDate(context),
+                        child: Text(
+                          ' ${DateFormat('dd-MM-yyyy').format(_selectedDate)}  ${DateFormat('EEEE').format(_selectedDate)}',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blue),
+                          elevation: MaterialStateProperty.all(
+                              0.0), // Remove elevation
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: _nextDate,
+                        icon: Icon(Icons.arrow_forward),
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: _eventsByDate.isEmpty
+                      ? Center(
+                          child: Text('No events for selected date'),
+                        )
+                      : ListView.builder(
+                          itemCount: _eventsByDate.length,
+                          itemBuilder: (context, index) {
+                            final eventByDate = _eventsByDate[index];
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemCount: eventByDate.events.length,
+                                  itemBuilder: (context, index) {
+                                    final event = eventByDate.events[index];
+                                    return Container(
+                                      margin: EdgeInsets.symmetric(
+                                          vertical: 5, horizontal: 10),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                          color: Colors.grey, // Border color
+                                          // Border width
+                                        ),
+                                      ),
+                                      child: ListTile(
+                                        title: Text(
+                                            '${event.eventName} - ${event.category}'),
+                                        subtitle: Text(
+                                          'Time: ${_formatTime(event.time)}',
+                                        ),
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ParticipantListScreen(
+                                                eventId: event.eventId,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                ),
+              ],
+            ),
+    );
+  }
+
   String _formatTime(String time) {
     // Convert the time string to a DateTime object
     DateTime parsedTime = DateFormat('HH:mm').parse(time);
@@ -36,146 +227,5 @@ class _EventListScreenState extends State<EventListScreen> {
     String formattedTime = DateFormat('h:mm a').format(parsedTime);
 
     return formattedTime;
-  }
-
-  // Function to get date part only
-  static DateTime dateOnly(DateTime date) {
-    return DateTime(date.year, date.month, date.day);
-  }
-
-  // Function to fetch events for a selected date
-  Future<List<Event>> _fetchEventsForSelectedDate(DateTime selectedDate) async {
-    _isLoading.value =
-        true; // Set loading flag to true when starting to fetch events
-    try {
-      final response =
-          await http.get(Uri.parse('http://10.0.2.2:3000/api/v1/events'));
-      if (response.statusCode == 200) {
-        final parsedResponse = json.decode(response.body);
-        final events = parsedResponse['data']['events'] as List;
-
-        // Filter events for the selected date
-        _events = events
-            .map((eventJson) => Event.fromJson(eventJson))
-            .where((event) => DateTime.parse(event.date)
-                .isAtSameMomentAs(dateOnly(_selectedDate)))
-            .toList();
-
-        _events.sort((a, b) => a.date.compareTo(b.date)); // Sort events by date
-        _newEvents = _events;
-      } else {
-        throw Exception('Failed to load events');
-      }
-    } catch (error) {
-      print('Error fetching events: $error');
-      // Handle error appropriately, e.g., show error message to user
-    }
-    _isLoading.value =
-        false; // Set loading flag to false after events are fetched
-    return _newEvents;
-  }
-
-  // Function to select a date
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate,
-      firstDate: DateTime(2020),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
-      _fetchEventsForSelectedDate(
-          _selectedDate); // Fetch events for newly selected date
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-        valueListenable: _isLoading,
-        builder: (BuildContext context, bool isLoading, Widget? child) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'Event List',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              backgroundColor: Colors.blue, // Adding color to AppBar
-            ),
-            body: isLoading
-                ? Center(
-                    child:
-                        CircularProgressIndicator()) // Display progress indicator while loading events
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () => _selectDate(context),
-                          child: Text(
-                            ' ${DateFormat('dd-MM-yyyy').format(_selectedDate)}  ${DateFormat('EEEE').format(_selectedDate)}',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                            ),
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blue),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: _newEvents.isEmpty
-                            ? Center(child: Text('No events for selected date'))
-                            : ListView.builder(
-                                itemCount: _newEvents.length,
-                                itemBuilder: (context, index) {
-                                  final event = _newEvents[index];
-                                  return GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                ParticipantListScreen()),
-                                      );
-                                    },
-                                    child: Container(
-                                      margin: EdgeInsets.symmetric(
-                                          vertical: 5, horizontal: 10),
-                                      decoration: BoxDecoration(
-                                        color:
-                                            Color.fromARGB(255, 203, 227, 247),
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      child: ListTile(
-                                        title: Text(
-                                            '${event.eventName} - ${event.category}'),
-                                        subtitle: Text(
-                                            'Time: ${_formatTime(event.time)}'),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                      ),
-                    ],
-                  ),
-          );
-        });
   }
 }
