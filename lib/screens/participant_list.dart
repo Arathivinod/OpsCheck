52d9de260c269519c9b47971b7255a8591b7ce7c
Arathivinod <arathivinodwrk@gmail.com>
2024-02-19 18:00:37 +0530

    mode selection feature added

--- lib/screens/participant_list.dart
+++ lib/screens/participant_list.dart
@@ -1,32 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
+import 'package:opscheck/modal/modal.dart';
+
+class ModeIcon extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String label;
+
+  const ModeIcon({
+    required this.icon,
+    required this.color,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          Icon(icon, color: color),
+          Text(label, style: TextStyle(color: color)),
+        ],
+      ),
+    );
+  }
+}
 
 class ParticipantListScreen extends StatefulWidget {
+  final int eventId;
+
+  ParticipantListScreen({required this.eventId});
+
   @override
   _ParticipantListScreenState createState() => _ParticipantListScreenState();
 }
 
 class _ParticipantListScreenState extends State<ParticipantListScreen> {
-  List<dynamic> participants = [];
+  List<Participant> _participants = [];
+  bool _isLoading = false;
+  String _eventName = ''; // Define _eventName variable
+  String _category = ''; // Define category variable
 
   @override
   void initState() {
     super.initState();
-    fetchParticipants();
+    _fetchParticipants();
   }
 
-  Future<void> fetchParticipants() async {
-    final response =
-        await http.get(Uri.parse('http://10.0.2.2:3000/api/v1/participants'));
+  Future<void> _fetchParticipants() async {
+    setState(() {
+      _isLoading = true;
+    });
 
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
+    try {
+      final response = await http.get(
+          Uri.parse('http://10.0.2.2:3000/api/v1/analytics/${widget.eventId}'));
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final List<dynamic> participantsData =
+            jsonData['data']['event']['participants'];
+
+        setState(() {
+          _eventName = jsonData['data']['event']['eventName'];
+          _category = jsonData['data']['event']['category'];
+
+          _participants = participantsData
+              .map((data) => Participant(
+                    participantId: data['participantId'],
+                    participantName: data['participantName'],
+                    participationMode: data['eventparticipantmapping']
+                        ['participationMode'],
+                  ))
+              .toList();
+          _isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load participants');
+      }
+    } catch (error) {
+      print('Error fetching participants: $error');
       setState(() {
-        participants = jsonData['data']['participants'];
+        _isLoading = false;
       });
-    } else {
-      throw Exception('Failed to load participants');
+    }
+  }
+
+  Future<void> _updateParticipationMode(
+      int participantId, int mode, DateTime date) async {
+    try {
+      final response = await http.patch(
+        Uri.parse('http://10.0.2.2:3000/api/v1/analytics/${widget.eventId}'),
+        body: json.encode({
+          'participantId': participantId, // Add participantId to the body
+          'participationMode': mode, // Correct the key to 'participationMode'
+          'date': DateFormat('yyyy-MM-dd').format(date)
+        }),
+        headers: {'Content-Type': 'application/json'},
+      );
+      if (response.statusCode == 200) {
+        _fetchParticipants();
+      } else {
+        throw Exception('Failed to update participation mode');
+      }
+    } catch (error) {
+      print('Error updating participation mode: $error');
     }
   }
 
@@ -35,31 +115,130 @@ class _ParticipantListScreenState extends State<ParticipantListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Participant List',
+          'Participants',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue, // App bar color
+        backgroundColor: Colors.blue,
       ),
-      body: participants.isEmpty
-          ? Center(child: CircularProgressIndicator()) // Show progress indicator if data is loading
-          : ListView.builder(
-              itemCount: participants.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  color: Color.fromARGB(255, 203, 227, 247), // Card color
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              '$_eventName - $_category',
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          ModeIcon(
+                            icon: Icons.work,
+                            color: Colors.blue,
+                            label: 'Office',
+                          ),
+                          ModeIcon(
+                            icon: Icons.home,
+                            color: Colors.green,
+                            label: 'WFH',
+                          ),
+                          ModeIcon(
+                            icon: Icons.not_interested,
+                            color: Colors.red,
+                            label: 'Absent',
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  child: ListTile(
-                    title: Text(
-                      participants[index]['participantName'],
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text('ID: ${participants[index]['participantId']}'),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _participants.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final participant = _participants[index];
+                      return Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.blue,
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: ListTile(
+                                title: Text(participant.participantName),
+                                subtitle:
+                                    Text('ID: ${participant.participantId}'),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5),
+                              child: IconButton(
+                                icon: Icon(Icons.work,
+                                    color: participant.participationMode == 1
+                                        ? Colors.blue
+                                        : Colors.grey),
+                                onPressed: () {
+                                  _updateParticipationMode(
+                                      participant.participantId,
+                                      1,
+                                      DateTime.now());
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5),
+                              child: IconButton(
+                                icon: Icon(Icons.home,
+                                    color: participant.participationMode == 2
+                                        ? Colors.green
+                                        : Colors.grey),
+                                onPressed: () {
+                                  _updateParticipationMode(
+                                      participant.participantId,
+                                      2,
+                                      DateTime.now());
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 6),
+                              child: IconButton(
+                                icon: Icon(Icons.not_interested,
+                                    color: participant.participationMode == 3
+                                        ? Colors.red
+                                        : Colors.grey),
+                                onPressed: () {
+                                  _updateParticipationMode(
+                                      participant.participantId,
+                                      3,
+                                      DateTime.now());
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
+                ),
+              ],
             ),
     );
   }
