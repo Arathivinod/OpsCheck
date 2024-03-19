92a0ee7f0f87cb18dfd89cd6252b84126ebe34f8
Arathivinod <arathivinodwrk@gmail.com>
2024-03-19 17:47:44 +0530

    imported localization packages and used them in Material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:opscheck/screens/loginpage.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale("en"),
       title: 'OpsCheck',
       home: const LoginScreen(),
       theme: ThemeData(
